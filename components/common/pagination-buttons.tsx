a65f1580de3e81925d369253224a34b4a6ea8b7d
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-08-15 14:23:29 +0700

    feat: mobile and desktop view

--- components/common/pagination-buttons.tsx
+++ components/common/pagination-buttons.tsx
@@ -19,84 +19,86 @@ const PaginationButtons: ComponentType<
   const hasNext = currentPage < length;
 
   return (
-    <section className="btn-group justify-end" {...rest}>
-      <button
-        onClick={() => onChangePage(1)}
-        disabled={!hasPrev}
-        className="btn btn-secondary"
-      >
-        <ChevronDoubleLeftIcon className="h-5 w-5" />
-      </button>
-      <button
-        onClick={() => onChangePage(currentPage - 1)}
-        disabled={!hasPrev}
-        className="btn btn-secondary"
-      >
-        <ChevronLeftIcon className="h-5 w-5" />
-      </button>
+    <>
+      {/* Mobile */}
+      <section className="btn-group flex justify-end sm:hidden" {...rest}>
+        <button
+          onClick={() => onChangePage(currentPage - 1)}
+          disabled={!hasPrev}
+          className="btn btn-secondary"
+        >
+          <ChevronLeftIcon className="h-5 w-5" />
+        </button>
 
-      <section className="form-control mx-4">
-        <div className="input-group">
-          <select
-            onChange={(e) => onChangePage(Number(e.target.value))}
-            className="select select-bordered"
-          >
-            {Array.from({ length }).map((_, idx) => {
-              const page = idx + 1;
-              return (
-                <option key={page} value={page} selected={currentPage === page}>
-                  {page}
-                </option>
-              );
-            })}
-          </select>
-          <button className="btn btn-secondary">of {length}</button>
-        </div>
+        <button className="btn btn-ghost">
+          {currentPage} / {length}
+        </button>
+
+        <button
+          onClick={() => onChangePage(currentPage + 1)}
+          disabled={!hasNext}
+          className="btn btn-secondary"
+        >
+          <ChevronRightIcon className="h-5 w-5" />
+        </button>
       </section>
 
-      <button
-        onClick={() => onChangePage(currentPage + 1)}
-        disabled={!hasNext}
-        className="btn btn-secondary"
-      >
-        <ChevronRightIcon className="h-5 w-5" />
-      </button>
-      <button
-        onClick={() => onChangePage(length)}
-        disabled={!hasNext}
-        className="btn btn-secondary"
-      >
-        <ChevronDoubleRightIcon className="h-5 w-5" />
-      </button>
-
-      {/* <button type="button" onClick={() => onChangePage(1)} className="btn">
-        1
-      </button>
-
-      {Array.from({ length: buttonsCount }).map((_, idx) => {
-        const number = idx + 1;
-        return (
-          <button
-            key={number}
-            type="button"
-            onClick={() => onChangePage(number)}
-            className={`btn ${currentPage === number && "btn-active"}`}
-          >
-            {number}
-          </button>
-        );
-      })}
-
-      {length && length > 1 && (
+      {/* Desktop */}
+      <section className="btn-group hidden justify-end sm:flex" {...rest}>
         <button
-          type="button"
-          onClick={() => onChangePage(length ?? 1)}
-          className="btn"
+          onClick={() => onChangePage(1)}
+          disabled={!hasPrev}
+          className="btn btn-secondary"
         >
-          {length}
+          <ChevronDoubleLeftIcon className="h-5 w-5" />
         </button>
-      )} */}
-    </section>
+        <button
+          onClick={() => onChangePage(currentPage - 1)}
+          disabled={!hasPrev}
+          className="btn btn-secondary"
+        >
+          <ChevronLeftIcon className="h-5 w-5" />
+        </button>
+
+        <section className="form-control mx-4">
+          <div className="input-group">
+            <select
+              onChange={(e) => onChangePage(Number(e.target.value))}
+              className="select select-bordered"
+            >
+              {Array.from({ length }).map((_, idx) => {
+                const page = idx + 1;
+                return (
+                  <option
+                    key={page}
+                    value={page}
+                    selected={currentPage === page}
+                  >
+                    {page}
+                  </option>
+                );
+              })}
+            </select>
+            <button className="btn btn-secondary">of {length}</button>
+          </div>
+        </section>
+
+        <button
+          onClick={() => onChangePage(currentPage + 1)}
+          disabled={!hasNext}
+          className="btn btn-secondary"
+        >
+          <ChevronRightIcon className="h-5 w-5" />
+        </button>
+        <button
+          onClick={() => onChangePage(length)}
+          disabled={!hasNext}
+          className="btn btn-secondary"
+        >
+          <ChevronDoubleRightIcon className="h-5 w-5" />
+        </button>
+      </section>
+    </>
   );
 };
 
