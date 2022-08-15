13ea055ba4296af6544610c0b98b697612d2906c
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-08-16 06:55:57 +0700

    fix: sometimes service does not use token due to useEffect slow re-render

--- libs/hooks/common/use-service.ts
+++ libs/hooks/common/use-service.ts
@@ -1,22 +1,10 @@
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
 
 interface CanBeInstantiated<T> {
   new (accessToken?: string): T;
 }
 
 export default function useService<T>(serviceType: CanBeInstantiated<T>): T {
-  const { data, status } = useSession();
-  const [service, setService] = useState(new serviceType());
-
-  useEffect(() => {
-    if (status === "authenticated") {
-      const user = data.user;
-      if (user.access_token) {
-        setService(new serviceType(user.access_token));
-      }
-    }
-  }, [data?.user, serviceType, status]);
-
-  return service as T;
+  const { data } = useSession();
+  return new serviceType(data?.user.access_token) as T;
 }
