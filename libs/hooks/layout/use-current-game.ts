3249ca51767c1d2f48ad1ec8229de0876e514052
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-08-16 06:57:14 +0700

    fix: use conditional data fetching with swr

--- libs/hooks/layout/use-current-game.ts
+++ libs/hooks/layout/use-current-game.ts
@@ -1,21 +1,20 @@
-import { getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import GameService from "../../services/game-service";
+import useService from "../common/use-service";
 
 export default function useCurrentGame() {
   const router = useRouter();
+  const { status } = useSession();
+  const gameService = useService(GameService);
+
   const gameId = router.query.gameId;
 
-  const { data, error } = useSWR(`/games/${gameId}`, async () => {
-    if (!gameId) {
-      return;
-    }
-
-    const session = await getSession();
-    const gameService = new GameService(session?.user.access_token);
-    return await gameService.getOneById(`${router.query.gameId}`);
-  });
+  const { data, error } = useSWR(
+    status === "authenticated" && gameId ? `/games/${gameId}` : null,
+    async () => await gameService.getOneById(`${router.query.gameId}`)
+  );
 
   return {
     game: data,
