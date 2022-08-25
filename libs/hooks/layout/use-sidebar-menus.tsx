6cbf06b30845e94fbfe7fffcdca4705d9a89a90e
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-08-25 14:38:07 +0700

    refactor: no use effect

--- libs/hooks/layout/use-sidebar-menus.tsx
+++ libs/hooks/layout/use-sidebar-menus.tsx
@@ -1,16 +1,8 @@
 import {
-  ClipboardIcon,
-  FlagIcon,
-  UserGroupIcon,
-  ClockIcon,
-  UsersIcon,
-  PhotographIcon,
-  ChartBarIcon,
-  CollectionIcon,
+  ChartBarIcon, ClipboardIcon, ClockIcon, CollectionIcon, FlagIcon, PhotographIcon, UserGroupIcon, UsersIcon
 } from "@heroicons/react/outline";
 import { FilmIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
 import Game from "../../models/game";
 
 interface IMenu {
@@ -22,88 +14,74 @@ interface IMenu {
 
 type MenuItem = IMenu | null;
 
-export default function useSidebarMenus(game?: Game) {
+export default function useSidebarMenus(game?: Game): MenuItem[] {
   const router = useRouter();
-  const [menus, setMenus] = useState<MenuItem[]>([]);
 
-  useEffect(() => {
-    const gameId = game?.id;
+  if (!game) return [];
 
-    if (!gameId) {
-      setMenus([]);
-      return;
-    }
+  const gameId = game?.id;
 
-    const originalMenus: MenuItem[] = [
-      {
-        label: "Details",
-        path: `/games/${gameId}/edit`,
-        icon: <ClipboardIcon className="h-6 w-6" />,
-        isActive: false,
-      },
-      {
-        label: "Missions",
-        path: `/games/${gameId}/missions`,
-        icon: <FlagIcon className="h-6 w-6" />,
-        isActive: false,
-      },
-      {
-        label: "Automations",
-        path: `/games/${gameId}/automations`,
-        icon: <FilmIcon className="h-6 w-6" />,
-        isActive: false,
-      },
-      {
-        label: "Participants",
-        path: `/games/${gameId}/participants`,
-        icon: <UserGroupIcon className="h-6 w-6" />,
-        isActive: false,
-      },
-      {
-        label: "Start & End",
-        path: `/games/${gameId}/start-end`,
-        icon: <ClockIcon className="h-6 w-6" />,
-        isActive: false,
-      },
-      {
-        label: "Admins",
-        path: `/games/${gameId}/admins`,
-        icon: <UsersIcon className="h-6 w-6" />,
-        isActive: false,
-      },
+  return [
+    {
+      label: "Details",
+      path: `/games/${gameId}/edit`,
+      icon: <ClipboardIcon className="h-6 w-6" />,
+      isActive: false,
+    },
+    {
+      label: "Missions",
+      path: `/games/${gameId}/missions`,
+      icon: <FlagIcon className="h-6 w-6" />,
+      isActive: false,
+    },
+    {
+      label: "Automations",
+      path: `/games/${gameId}/automations`,
+      icon: <FilmIcon className="h-6 w-6" />,
+      isActive: false,
+    },
+    {
+      label: "Participants",
+      path: `/games/${gameId}/participants`,
+      icon: <UserGroupIcon className="h-6 w-6" />,
+      isActive: false,
+    },
+    {
+      label: "Start & End",
+      path: `/games/${gameId}/start-end`,
+      icon: <ClockIcon className="h-6 w-6" />,
+      isActive: false,
+    },
+    {
+      label: "Admins",
+      path: `/games/${gameId}/admins`,
+      icon: <UsersIcon className="h-6 w-6" />,
+      isActive: false,
+    },
 
-      null,
+    null,
 
-      {
-        label: "Activity Feed",
-        path: `/games/${gameId}/feed`,
-        icon: <CollectionIcon className="h-6 w-6" />,
-        isActive: false,
-      },
-      {
-        label: "Leaderboard",
-        path: `/games/${gameId}/leaderboard`,
-        icon: <ChartBarIcon className="h-6 w-6" />,
-        isActive: false,
-      },
-      {
-        label: "Submissions",
-        path: `/games/${gameId}/submissions`,
-        icon: <PhotographIcon className="h-6 w-6" />,
-        isActive: false,
-      },
-    ];
-
-    setMenus(
-      originalMenus.map((menu) => {
-        if (!menu) return menu;
-        const sanitizedPath = new URL(menu.path, location.href).pathname;
-        const sanitizedAsPath = new URL(router.asPath, location.href).pathname;
-        menu.isActive = sanitizedAsPath.startsWith(sanitizedPath);
-        return menu;
-      })
-    );
-  }, [game, router.asPath]);
-
-  return menus;
+    {
+      label: "Activity Feed",
+      path: `/games/${gameId}/feed`,
+      icon: <CollectionIcon className="h-6 w-6" />,
+      isActive: false,
+    },
+    {
+      label: "Leaderboard",
+      path: `/games/${gameId}/leaderboard`,
+      icon: <ChartBarIcon className="h-6 w-6" />,
+      isActive: false,
+    },
+    {
+      label: "Submissions",
+      path: `/games/${gameId}/submissions`,
+      icon: <PhotographIcon className="h-6 w-6" />,
+      isActive: false,
+    },
+  ].map((menu) => {
+    if (!menu) return menu;
+    menu.isActive = router.asPath.startsWith(menu.path);
+    return menu;
+  });
 }
