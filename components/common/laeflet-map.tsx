5ae9eef21a634b18ecbc3c307d8722b35a9db57b
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-08-25 16:11:45 +0700

    fix: dynamic import component cannot be passed ref, so we must handle default lat lang in the LeafletMap component instead

--- components/common/laeflet-map.tsx
+++ components/common/laeflet-map.tsx
@@ -1,13 +1,9 @@
+import { LeafletEventHandlerFnMap, Map } from "leaflet";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet/dist/leaflet.css";
-import { LeafletEventHandlerFnMap, Map } from "leaflet";
-import {
-  ComponentProps,
-  ComponentType,
-  forwardRef,
-  MutableRefObject,
-} from "react";
+import { ComponentProps, ComponentType, useRef } from "react";
+import toast from "react-hot-toast";
 import {
   Circle,
   MapContainer,
@@ -17,28 +13,39 @@ import {
 } from "react-leaflet";
 
 interface ILeafletMap {
-  latitude: number;
-  longitude: number;
+  latitude?: number;
+  longitude?: number;
   circleRadius?: number;
   onLatLngChange?: (lat: number, lng: number) => void;
   scrollWheelZoom?: boolean;
   draggable?: boolean;
 }
 
+const DEFAULT_LATITUDE = 51.505;
+const DEFAULT_LONGITUDE = -0.09;
+
 const LeafletMap: ComponentType<
   ComponentProps<typeof MapContainer> & ILeafletMap
-> = (
-  {
-    latitude,
-    longitude,
-    circleRadius,
-    onLatLngChange,
-    scrollWheelZoom,
-    draggable,
-    ...rest
-  },
-  ref: MutableRefObject<Map>
-) => {
+> = ({
+  latitude,
+  longitude,
+  circleRadius,
+  onLatLngChange,
+  scrollWheelZoom,
+  draggable,
+  ...rest
+}) => {
+  const leafletMapRef = useRef<Map | null>(null);
+
+  const originalLatitude = latitude;
+  const originalLongitude = longitude;
+
+  latitude = originalLatitude ?? DEFAULT_LATITUDE;
+  longitude = originalLongitude ?? DEFAULT_LONGITUDE;
+
+  const invalidCoordinate = !originalLatitude || !originalLongitude;
+  const hasNavigator = typeof navigator !== "undefined";
+
   const eventHandlers: LeafletEventHandlerFnMap = {
     drag: (e) => {
       const target = e.target as L.Marker;
@@ -47,9 +54,23 @@ const LeafletMap: ComponentType<
     },
   };
 
+  if (invalidCoordinate && hasNavigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        leafletMapRef.current?.panTo({ lat: latitude, lng: longitude });
+        onLatLngChange && onLatLngChange(latitude, longitude);
+      },
+      (err) => {
+        toast.error(`${err.code}: ${err.message}`);
+        console.error(err);
+      }
+    );
+  }
+
   return (
     <MapContainer
-      ref={ref}
+      ref={leafletMapRef}
       center={[latitude, longitude]}
       zoom={16}
       className="h-96"
@@ -94,4 +115,4 @@ const MapContainerEventListeners: ComponentType<
 };
 
 // @ts-ignore: has type error, but hey, as long as it works
-export default forwardRef(LeafletMap);
+export default LeafletMap;
--- components/mission/form/mission-form-gps-type-map.tsx
+++ components/mission/form/mission-form-gps-type-map.tsx
@@ -1,8 +1,6 @@
-import { Map } from "leaflet";
 import dynamic from "next/dynamic";
-import { ComponentType, useRef } from "react";
+import { ComponentType } from "react";
 import { UseFormSetValue, UseFormWatch } from "react-hook-form";
-import toast from "react-hot-toast";
 import { MissionFormValues } from "./mission-form";
 
 const LeafletMap = dynamic(() => import("../../common/laeflet-map"), {
@@ -14,21 +12,13 @@ interface IMissionFormGpsTypeMap {
   setValue: UseFormSetValue<MissionFormValues>;
 }
 
-const DEFAULT_LATITUDE = 51.505;
-const DEFAULT_LONGITUDE = -0.09;
-
 const MissionFormGpsTypeMap: ComponentType<IMissionFormGpsTypeMap> = ({
   watch,
   setValue,
 }) => {
-  const leafletMapRef = useRef<Map | null>(null);
-
-  const originalLatitude = watch("mission_data.latitude");
-  const originalLongitude = watch("mission_data.longitude");
+  const latitude = watch("mission_data.latitude");
+  const longitude = watch("mission_data.longitude");
   const radius = watch("mission_data.radius");
-
-  const invalidCoordinate = !originalLatitude || !originalLongitude;
-  const hasNavigator = typeof navigator !== "undefined";
 
   const onLatLngChange = (lat: number, long: number) => {
     const reRenderOptions = {
@@ -40,26 +30,8 @@ const MissionFormGpsTypeMap: ComponentType<IMissionFormGpsTypeMap> = ({
     setValue("mission_data.longitude", long, reRenderOptions);
   };
 
-  if (invalidCoordinate && hasNavigator) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        leafletMapRef.current?.panTo({ lat: latitude, lng: longitude });
-        onLatLngChange(latitude, longitude);
-      },
-      (err) => {
-        toast.error(`${err.code}: ${err.message}`);
-        console.error(err);
-      }
-    );
-  }
-
-  const latitude = originalLatitude ?? DEFAULT_LATITUDE;
-  const longitude = originalLongitude ?? DEFAULT_LONGITUDE;
-
   return (
     <LeafletMap
-      ref={leafletMapRef}
       latitude={latitude}
       longitude={longitude}
       circleRadius={radius}
