b7045fc3fc42864eb5beaf6b078690bb7d96fcb7
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-09-02 15:52:45 +0700

    style: format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,10 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
           name="description"
           content="Complete your missions in BINUS University!"
         />
-        <link rel="icon" href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon.ico`} />
+        <link
+          rel="icon"
+          href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon.ico`}
+        />
       </Head>
 
       <RedirectIfUnauthenticated />
