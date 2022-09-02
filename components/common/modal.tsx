9d95b7b0471832322341cf2cd8d96bf3d13d045c
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-09-02 15:53:09 +0700

    feat: base modal for confirmation dialog

---
+++ components/common/modal.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps, ComponentType } from "react";
+
+interface IModal {
+  modalKey: string;
+  title?: string;
+}
+
+const Modal: ComponentType<ComponentProps<"div"> & IModal> = ({
+  modalKey,
+  title,
+  children,
+  className,
+  ...rest
+}) => {
+  return (
+    <>
+      <input type="checkbox" id={modalKey} className="modal-toggle" />
+
+      <div className={`modal ${className}`} {...rest}>
+        <div className="modal-box">
+          {title && <h3 className="text-lg font-bold">{title}</h3>}
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
