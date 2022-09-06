a5f5bbee7e5c55785f071bb976294c569dab9253
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-09-06 09:37:28 +0700

    feat: shut up the bundle analyzer

--- next.config.js
+++ next.config.js
@@ -36,6 +36,7 @@ const nextConfig = {
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
+  openAnalyzer: false,
 });
 
 module.exports = withBundleAnalyzer(nextConfig);
